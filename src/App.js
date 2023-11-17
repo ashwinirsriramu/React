6fca10b489764d7e3ba9b5a6c831f6e459e7fb5d
ashwinirsriramu <ashwinirsriramu@gmail.com>
2023-11-17 20:13:32 +0530

    one

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from './Components/Header'
+import Body from './Components/Body'
+import Footer from './Components/Footer'
 const App=()=> {
   return (
    <div>
    <Header/>
+   <Body/>
+   <Footer/>
    
    </div>
   )
